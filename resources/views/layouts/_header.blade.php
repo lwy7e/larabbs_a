54120a924046f20fafcbbb032b7c3d6a0856dce3
lwy7e <954924545@qq.com>
2019-07-13 17:40:09 +0800

    基础页面结构

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,32 +1,24 @@
-<nav class="navbar navbar-default navbar-static-top">
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
   <div class="container">
-    <div class="navbar-header">
+    <!-- Branding Image -->
+    <a class="navbar-brand " href="{{ url('/') }}">
+      LaraBBS
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-      <!-- Collapsed Hamburger -->
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-        <span class="sr-only">Toggle Navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-
-      <!-- Branding Image -->
-      <a class="navbar-brand" href="{{ url('/') }}">
-        LaraBBS
-      </a>
-    </div>
-
-    <div class="collapse navbar-collapse" id="app-navbar-collapse">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <!-- Left Side Of Navbar -->
-      <ul class="nav navbar-nav">
+      <ul class="navbar-nav mr-auto">
 
       </ul>
 
       <!-- Right Side Of Navbar -->
-      <ul class="nav navbar-nav navbar-right">
+      <ul class="navbar-nav navbar-right">
         <!-- Authentication Links -->
-        <li><a href="#">登录</a></li>
-        <li><a href="#">注册</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
+        <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
       </ul>
     </div>
   </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,39 @@
-<!doctype html>
-<html lang="{{app()->getLocale()}}">
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
 <head>
-  <meta charset="UTF-8">
+  <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- CSRF token --->
+  <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>@yield('title','LaraBBS')-Laravel </title>
+  <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
 
-  <!-- Style-->
-  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <!-- Styles -->
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
 </head>
+
 <body>
 <div id="app" class="{{ route_class() }}-page">
+
   @include('layouts._header')
 
-  <div>
+  <div class="container">
+
+    @include('shared._messages')
 
     @yield('content')
 
   </div>
-  @include('layouts._footer')
 
+  @include('layouts._footer')
 </div>
 
-<!---javaScript --->
-<script  src="{{ asset('js/app.js') }}"></script>
+<!-- Scripts -->
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
-</html>
 
-  
+</html>
