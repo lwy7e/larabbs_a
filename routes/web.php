1f9200a95f2905b1e52f39bad36b89b49421e86e
lwy7e <954924545@qq.com>
2019-07-22 11:40:36 +0800

    '个人中心'

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@
     Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
     Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
     Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+//
+    Route::resource('users','UsersController',['only'=>['show','update','edit']]);
