d8fa7d18fa3ce027cbd53e5644313dfceb90aacf
lwy7e <954924545@qq.com>
2019-09-06 16:20:47 +0800

    填充用户和话题数据

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,7 +1,6 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-use App\Models\User;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -16,12 +15,17 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
-    ];
-});
+
+    $factory->define(App\Models\User::class, function (Faker $faker) {
+        $date_time = $faker->date . ' ' . $faker->time;
+        return [
+            'name' => $faker->name,
+            'email' => $faker->unique()->safeEmail,
+            'remember_token' => Str::random(10),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'introduction' => $faker->sentence(),
+            'created_at' => $date_time,
+            'updated_at' => $date_time,
+        ];
+    });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,17 +1,1 @@
-<?php
-
-use Illuminate\Database\Seeder;
-
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-		$this->call(TopicsTableSeeder::class);
-    }
-}
+<?php    use Illuminate\Database\Seeder;    class DatabaseSeeder extends Seeder    {        public function run()        {            $this->call(UsersTableSeeder::class);            $this->call(TopicsTableSeeder::class);        }    }
