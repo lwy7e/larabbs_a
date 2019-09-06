6f281e81471b0d100ca2ee247e0d8ca15c4ef4cd
lwy7e <954924545@qq.com>
2019-09-06 17:28:28 +0800

    分类话题列表

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Topic;
+
+
+class CategoriesController extends Controller
+{
+    //
+    public function show(Category $category)
+    {
+        //读取分类 ID 关联的话题,并按没20条分页
+
+        $topics = Topic::where('category_id',$category->id)->paginate(20);
+
+        // 传参变量话题和分类到模板中
+        return view('topics.index',compact('topics','category'));
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,8 @@
     Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
 //
-    Route::resource('users','UsersController',['only'=>['show','update','edit']]);
+    Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+    Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+    Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
