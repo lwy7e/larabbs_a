40a2e7ec39f988f2753cbac25594f419c7a50708
lwy7e <954924545@qq.com>
2019-09-27 17:57:44 +0800

    翻译 URL 标示

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -51,7 +51,10 @@ class Topic extends Model
 
     }
 
-
+    public function link($params = [])
+    {
+        return route('topics.show', array_merge([$this->id, $this->slug], $params));
+    }
 
 }
 
--- routes/web.php
+++ routes/web.php
@@ -24,9 +24,11 @@
 //
     Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-    Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+    Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
 
     Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
     //上传图片
     Route::post('upload_image','TopicsController@uploadImage')->name('topics.upload_image');
+
+    Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
