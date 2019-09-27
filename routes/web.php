0d31b3a266f1135b061c31ced9c0422fc8ed8e51
lwy7e <954924545@qq.com>
2019-09-27 14:27:25 +0800

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,6 @@
     Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
     Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+
+    //上传图片
+    Route::post('upload_image','TopicsController@uploadImage')->name('topics.upload_image');
