4566fe6329a38e0426c1c6939aaddb848e748d0b
luwenyi <954924545@qq.com>
2019-10-14 11:09:25 +0800

    注释 sudosu先

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,9 +32,9 @@
 
   @include('layouts._footer')
 </div>
-  @if (app()->isLocal())
-    @include('sudosu::user-selector')
-  @endif
+{{--  @if (app()->isLocal())--}}
+{{--    @include('sudosu::user-selector')--}}
+{{--  @endif--}}
 <!-- Scripts -->
 <script src="{{ mix('js/app.js') }}"></script>
 
